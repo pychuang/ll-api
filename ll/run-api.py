0a3f02e1406e22bb9504ed35b2137166546ef5cc
Anne Schuth <anne.schuth@gmail.com>
2014-04-26 13:40:14 +0400

    moved run files out of module

--- ll/run-api.py
+++
@@ -1,6 +0,0 @@
-from api import app
-from api.participant import *
-from api.site import *
-
-if __name__ == '__main__':
-    app.run(debug = True)
---
+++ run-api.py
@@ -0,0 +1,15 @@
+import argparse
+from ll.api import app
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Living Labs Challenge's API Server")
+    parser.add_argument('--debug', dest='debug', action='store_true',
+                        help='Enable debugging mode.')
+    parser.add_argument('--host', dest='host', default='127.0.0.1',
+                        help='Host to listen on.')
+    parser.add_argument('--port', dest='port', default=5000, type=int,
+                        help='Port to listen on.')
+    args = parser.parse_args()
+    app.debug = args.debug
+    print(" * Living Labs Challenge API")
+    app.run(host=args.host, port=args.port)
