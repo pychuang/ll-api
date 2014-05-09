3732a99e6e345559e6b8f36abdb9dc12a2ec7d75
Anne Schuth <anne.schuth@gmail.com>
2014-05-09 16:45:38 +0400

    Added license to each file

--- ll/core/run.py
+++ ll/core/run.py
@@ -1,3 +1,18 @@
+# This file is part of Living Labs Challenge, see http://living-labs.net.
+#
+# Living Labs Challenge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Living Labs Challenge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Living Labs Challenge. If not, see <http://www.gnu.org/licenses/>.
+
 from db import db
 import random
 import site
