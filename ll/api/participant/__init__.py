ff592baeba93dff10c5fcd72e50d1ee4b44656fb
Anne Schuth <anne.schuth@gmail.com>
2014-08-17 15:22:36 +0100

    removed the key endpoint, no longer used!

--- ll/api/participant/__init__.py
+++ ll/api/participant/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Living Labs Challenge. If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ["key", "query", "doc", "run", "feedback"]
+__all__ = ["query", "doc", "run", "feedback"]
 from .. import core
 from .. import app
 from . import *
