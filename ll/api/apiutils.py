83f4543ab8c6d45a34ad992ff196ec09a1847675
Anne Schuth <anne.schuth@gmail.com>
2014-09-06 18:16:05 +0100

    Cleaned up aborting and the traceback

--- ll/api/apiutils.py
+++ ll/api/apiutils.py
@@ -17,8 +17,6 @@ import traceback
 from flask.ext.restful import Resource, abort, fields
 from .. import core
 
-DOCUMENTATION = "http://doc.living-labs.net/"
-
 
 class ContentField(fields.Raw):
     def format(self, value):
@@ -26,46 +24,52 @@ class ContentField(fields.Raw):
 
 
 class ApiResource(Resource):
+    def abort(self, status, message, tb=None):
+        if tb is None:
+            abort(status,
+                  message=str(message).strip().strip("."),
+                  status=status,
+                  documentaton=core.config.config["URL_DOC"])
+        else:
+            abort(status,
+                  message=str(message).strip().strip("."),
+                  status=status,
+                  documentaton=core.config.config["URL_DOC"],
+                  traceback=[l for l in tb.split("\n") if l])
+
     def check_fields(self, o, fields):
         notfound = [f for f in fields if f not in o]
         if notfound:
-            abort(400, message="Please specify field(s): '%s'. See %s." %
-                      (", ".join(notfound), DOCUMENTATION))
+            self.abort(400, "Please specify field(s): '%s'" %
+                       ", ".join(notfound))
 
     def trycall(self, function, *args, **kwargs):
-        #function(*args, **kwargs)
         try:
             return function(*args, **kwargs)
         except ValueError, e:
-            tb = traceback.format_exc()
-            abort(409, message=str(e).strip() + ". See %s." % DOCUMENTATION,
-                  traceback=tb)
+            self.abort(409, e, traceback.format_exc())
         except LookupError, e:
-            tb = traceback.format_exc()
-            abort(404, message=str(e).strip() + ". See %s." % DOCUMENTATION,
-                  traceback=tb)
+            self.abort(404, e, traceback.format_exc())
         except Exception, e:
-            tb = traceback.format_exc()
-            abort(400, message=str(e).strip() + ". See %s." % DOCUMENTATION,
-                  traceback=tb)
+            self.abort(400, e, traceback.format_exc())
 
     def get_site_id(self, key):
         user = self.trycall(core.user.get_user, key)
         if not user:
-            abort(403, message="No such key. See %s." % DOCUMENTATION)
+            self.abort(403, "No such key")
         if not user["is_site"]:
-            abort(403, message="Not a site. Please use the participant "
-                  "API instead. See %s." % DOCUMENTATION)
+            self.abort(403, "Not a site. Please use the participant "
+                       "API instead")
         return user["site_id"]
 
     def validate_participant(self, key):
         user = self.trycall(core.user.get_user, key)
         if not user:
-            abort(403, message="No such key. See %s." % DOCUMENTATION)
+            self.abort(403, "No such key")
         if not user["is_participant"]:
-            abort(403, message="Not a participant. Please use the site "
-                  "API instead. See %s." % DOCUMENTATION)
+            self.abort(403, "Not a participant. Please use the site "
+                       "API instead")
         if not user["is_verified"]:
-            abort(403, message="Not verified (yet). Please send the signed "
-                  "registration form.")
+            self.abort(403, "Not verified (yet). Please send the signed "
+                       "registration form.")
         return True
