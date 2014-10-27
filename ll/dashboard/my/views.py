d76c2314aa5920f897fdb7b191e6de602e11c5b1
Anne Schuth <anne.schuth@gmail.com>
2014-10-27 14:46:46 +0100

    Forgotten commits

---
+++ ll/dashboard/my/views.py
@@ -0,0 +1,27 @@
+# This file is part of Living Labs Challenge, see http://living-labs.net.
+#
+# Living Labs Challenge is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Living Labs Challenge is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Living Labs Challenge. If not, see <http://www.gnu.org/licenses/>.
+
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for
+import json
+from .. import core, requires_login
+
+mod = Blueprint('my', __name__, url_prefix='/my')
+
+
+@mod.route('/')
+@requires_login
+def my():
+    return render_template("my/my.html", user=g.user)
+
