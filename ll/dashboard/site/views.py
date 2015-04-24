322b8497b9df175c1c0d38f50b9ce1b4b8f0429f
Anne Schuth <anne.schuth@gmail.com>
2015-04-24 11:42:21 +0200

    restrict to single feedback

--- ll/dashboard/site/views.py
+++ ll/dashboard/site/views.py
@@ -70,7 +70,8 @@ def query(site_id):
 @requires_login
 def query_detail(site_id, qid):
     site = core.site.get_site(site_id)
-    historical = core.feedback.get_historical_feedback(qid=qid, site_id=site_id)
+    historical = core.feedback.get_historical_feedback(qid=qid,
+                                                       site_id=site_id)[0]
     return render_template("site/query_detail.html",
                            user=g.user,
                            site=site,
