917f31bf60d7f2d366c51a5b78ed6dbc0a7231b5
Anne Schuth <anne.schuth@gmail.com>
2015-04-24 15:04:52 +0200

    Fixed bug when no historical interaction data is present

--- ll/dashboard/site/views.py
+++ ll/dashboard/site/views.py
@@ -70,12 +70,12 @@ def query(site_id):
 @requires_login
 def query_detail(site_id, qid):
     site = core.site.get_site(site_id)
-    historical = core.feedback.get_historical_feedback(qid=qid,
-                                                       site_id=site_id)[0]
+    historicals = core.feedback.get_historical_feedback(qid=qid,
+                                                        site_id=site_id)
     return render_template("site/query_detail.html",
                            user=g.user,
                            site=site,
-                           historical=historical,
+                           historical=historicals[0] if historicals else None,
                            query=core.db.db.query.find_one({"site_id": site_id,
                                                             "_id": qid}))
 
