3ad7385e8e5206444efde09fc2fd91873e7cfe70
Anne Schuth <anne.schuth@gmail.com>
2015-07-22 11:22:22 +0200

    Added nose to requirements Made test.py PEP8 compatible Sped up testing by providing min/max waits

--- test/test.py
+++ test/test.py
@@ -15,12 +15,9 @@
 
 import unittest
 import subprocess
-import nose
 import signal
 import os
 import shutil
-
-import ll.core
 
 MONGOD_TEST_CONFIG = "config/mongodb_test.conf"
 LL_CONFIG = "config/livinglabs.local.ini"
@@ -39,12 +36,10 @@ DUMP_PATH = "dump"
 SITE_KEY = "E0016261DE4C0D61-M6C4AMHHE4WV4OVY"
 PARTICIPANT_KEY = "9EA887B684DD5822-JBB2XOCVEGYE7YAZ"
 HOST = "localhost"
-N_ITERATIONS = 3 # number of iterations site or participant runs
+N_ITERATIONS = 3  # number of iterations site or participant runs
 
 
-    
 class TestLL(unittest.TestCase):
-    
     def setUp(self):
         self.mongo_pid = 0
         self.api_process = None
@@ -55,8 +50,8 @@ class TestLL(unittest.TestCase):
         os.makedirs("testdb")
         # Launch mongod
         print("Launch MongoDB")
-        mongo_output = subprocess.check_output(["mongod","--fork","--syslog",
-                                                "--config",MONGOD_TEST_CONFIG])
+        mongo_output = subprocess.check_output(["mongod", "--fork", "--syslog",
+                                                "--config", MONGOD_TEST_CONFIG])
         # Real Mongo process will be forked, save for pid from output
         for word in mongo_output.split():
             if word.isdigit():
@@ -64,34 +59,34 @@ class TestLL(unittest.TestCase):
         # Set up MongoDB users
         print("Setup MongoDB users")
         subprocess.call(["./bin/admin", "db", "--setup-db-users",
-                            "--mongodb_db", DB_NAME, "--mongodb_user", USER,
-                            "--mongodb_user_pw", USER_PWD,
-                            "--mongodb_admin", ADMIN,
-                            "--mongodb_admin_pw", ADMIN_PWD])
+                         "--mongodb_db", DB_NAME, "--mongodb_user", USER,
+                         "--mongodb_user_pw", USER_PWD,
+                         "--mongodb_admin", ADMIN,
+                         "--mongodb_admin_pw", ADMIN_PWD])
         # Save to configuration file
         subprocess.call(["./bin/admin", "db", "--mongodb_db", DB_NAME,
-                        "--mongodb_user", USER, "--mongodb_user_pw", USER_PWD,
-                        "--export-conf-file", LL_DB_CONFIG])
+                         "--mongodb_user", USER, "--mongodb_user_pw", USER_PWD,
+                         "--export-conf-file", LL_DB_CONFIG])
         # Start the API
         self.api_process = subprocess.Popen(["./bin/api",
-                                            "-c",LL_CONFIG,LL_DB_CONFIG,
-                                            "--debug"])
+                                             "-c", LL_CONFIG, LL_DB_CONFIG,
+                                             "--debug"])
         # Fill database with users
-        subprocess.call(["./bin/admin","db","--import-json",DUMP_PATH,
-                            "-c", LL_DB_CONFIG])
-        
-    
+        subprocess.call(["./bin/admin", "db", "--import-json", DUMP_PATH,
+                         "-c", LL_DB_CONFIG])
+
     def test_site(self):
         print("Test client")
         subprocess.call(["./bin/client-site", "--host", HOST, "--key", SITE_KEY,
-                        "-q","-d"])
+                         "-q", "-d"])
         print("Simulate client")
         site_output = subprocess.check_output(["./bin/client-site",
-                                                "--host", "localhost",
-                                                "--key", SITE_KEY,
-                                                "-s",
-                                                "--iterations",
-                                                str(N_ITERATIONS)])
+                                               "--host", "localhost",
+                                               "--key", SITE_KEY,
+                                               "-s",
+                                               "--iterations", str(N_ITERATIONS),
+                                               "--wait_max", "0",
+                                               "--wait_min", "0"])
         for line in site_output.split("\n"):
             if len(line) > 0:
                 if line.split()[0] != "NDCG:":
@@ -100,15 +95,18 @@ class TestLL(unittest.TestCase):
     
     def test_participant(self):
         print("Simulate participant")
-        participant_output = subprocess.check_output(
-                        ["./bin/client-participant", "--host", HOST,
-                        "--key", PARTICIPANT_KEY, "-s",
-                        "--iterations", str(N_ITERATIONS)])
+        participant_output = subprocess.check_output(["./bin/client-participant",
+                                                      "--host", HOST,
+                                                      "--key", PARTICIPANT_KEY,
+                                                      "-s",
+                                                      "--iterations", str(N_ITERATIONS),
+                                                      "--wait_max", "0",
+                                                      "--wait_min", "0"])
         for line in participant_output.split("\n"):
             if len(line) > 0:
                 first = line[0]
                 last = line[-1]
-                if not (first=="{" and last=="}"):
+                if not (first == "{" and last == "}"):
                     raise ValueError("Unexpected participant output:", line)
         return True
     
@@ -118,11 +116,7 @@ class TestLL(unittest.TestCase):
         # Kill api
         self.api_process.kill()
         # Kill mongo
-        os.kill(self.mongo_pid,signal.SIGKILL)
+        os.kill(self.mongo_pid, signal.SIGKILL)
         
         # Remove testdb directory
         shutil.rmtree("testdb")
-        
-
-if __name__ == '__main__':
-    unittest.main()
